3ff54ad4a5fd2fd0c4ca52f9047d7e7fd73a4c47
Lincoln Thurlow <lincoln@isi.edu>
2018-04-27 16:15:20 -0700

    integration test: get initial telnet working

--- test/integration/telnet-client.go
+++ test/integration/telnet-client.go
@@ -1,11 +1,39 @@
 package main
 
 import (
-	"github.com/reiver/go-telnet"
+	"github.com/ziutek/telnet"
+	"log"
 )
 
 func main() {
-	var caller telnet.Caller = telnet.StandardCaller
-	//@TOOD: replace "example.net:5555" with address you want to connect to.
-	telnet.DialToAndCall("localhost:4007", caller)
+	t, err := telnet.Dial("tcp", "localhost:4007")
+	if err != nil {
+		log.Fatalf("%v", err)
+	}
+
+	buf := make([]byte, 512)
+
+	// set the mac address for the interface
+	buf = []byte("ip link set eth1 address 00:00:00:00:00:01\r\n")
+	n, err := t.Write(buf)
+	if err != nil {
+		log.Fatalf("%v", err)
+	}
+	log.Printf("%d: %s", n, buf[:n])
+
+	// show the link information
+	buf = []byte("ip link show\r\n")
+	n, err = t.Write(buf)
+	if err != nil {
+		log.Fatalf("%v", err)
+	}
+	log.Printf("%d: %s", n, buf[:n])
+	buf = []byte("")
+
+	// check the output of ip link show
+	buf, err = t.ReadUntil("%")
+	if err != nil {
+		log.Fatalf("%v", err)
+	}
+	log.Printf("%s", buf)
 }
