1906cde71fa9fd8c8dbfa2742cbc4e7c3fcb2ba0
Lincoln Thurlow <lincoln@isi.edu>
2018-04-29 15:10:46 -0700

    integration: testing full integration test

--- test/integration/integration_test.go
+++ test/integration/integration_test.go
@@ -2,70 +2,75 @@ package integration
 
 import (
 	"errors"
+	"fmt"
+	"github.com/ceftb/sled/test/integration/client"
+	"github.com/ceftb/sled/test/integration/server"
+	"github.com/ceftb/sled/test/integration/shared"
 	"github.com/rcgoodfellow/raven/rvn"
 	log "github.com/sirupsen/logrus"
 	"github.com/sparrc/go-ping" // pingwait
+	"os"
 	"testing"
 	"time"
 )
 
-func startRaven() error {
-	// lets start by creating rvn info
-	err := rvn.RunModel()
+func setupServer() error {
+	macAddr := shared.ClientMAC
+	iface := shared.ServerIface
+	ipAddr := shared.ServerIP
+
+	server.SetServerIface(iface)
+
+	success, link := server.CheckServerIface(iface)
+	if !success {
+		return errors.New(fmt.Sprintf("Server interface %s - wanted: UP, got: %v", iface, link))
+	}
+	server.SetServerIP(iface, ipAddr)
+
+	success, ip := server.CheckServerIP(iface, ipAddr)
+	if !success {
+		return errors.New(fmt.Sprintf("Server IP not set! wanted: %v, got: %v", ipAddr, ip))
+	}
+
+	_, err := os.Stat(shared.LocalBoltPath)
 	if err != nil {
-		return err
+		return errors.New(fmt.Sprintf("Bolt DB executable missing at: %s", shared.LocalBoltPath))
 	}
-	// do libvirt things, atm does not return error
-	rvn.Create()
 
-	// now we can start to use our nodes, bring raven online
-	strErr := rvn.Launch()
-	if len(strErr) != 0 {
-		for _, e := range strErr {
-			log.Println(e)
-		}
-		// TODO: another hack, just return the first of many errors
-		return errors.New(strErr[0])
+	success, boltEntry := server.SetAndCheckBoltDB(macAddr)
+	if !success {
+		return errors.New(fmt.Sprintf("Server BOLTDB not set! wanted: %v, got: %v", macAddr, boltEntry))
 	}
-	return nil
-
-	// wait until the nodes are ready to be setup
-	doPingwait()
-
-	// configure the nodes -- set up mounts, again no error
-	rvn.Configure(true)
-
-	// wait until the nodes are ready to be setup
-	doPingwait()
-
 	return nil
 }
 
-func stopRaven() error {
-	err := rvn.Shutdown()
-	if err != nil {
-		for _, e := range err {
-			log.Printf("%v", e)
-		}
-		// FIXME: short hack, just return first of the errors
-		return err[0]
+func setupClient() error {
+	macAddr := shared.ClientMAC
+	iface := shared.ClientIface
+	ipAddr := shared.ClientIP
+
+	client.SetClientMAC(iface, macAddr)
+	success, mac := client.CheckClientMAC(iface, macAddr)
+	if !success {
+		return errors.New(fmt.Sprintf("Client MAC not set! wanted: %v, got: %v", macAddr, mac))
+	}
+	client.SetClientIfaceUP(iface)
+
+	// test that the client interface is up
+	success, link := client.CheckClientIfaceUP(iface)
+	if !success {
+		return errors.New(fmt.Sprintf("Client interface %s - wanted: UP, got: %v", iface, link))
 	}
 
-	// remove all info/details on the raven test configuration
-	rvn.Destroy()
+	// set the client IP address
+	client.SetClientIP(iface, ipAddr)
+
+	//test that the client IP address was set correctly
+	success, ip := client.CheckClientIP(iface, ipAddr)
+	if !success {
+		return errors.New(fmt.Sprintf("Client IP not set! wanted: %v, got: %v", ip, ipAddr))
+	}
 	return nil
-}
-
-func getNodeIP(node string) string {
-	topo, err := rvn.LoadTopo()
-	if err != nil {
-		log.Fatal(err)
-	}
-	ds, err := rvn.DomainStatus(topo.Name, node)
-	if err != nil {
-		log.Fatal(err)
-	}
-	return ds.IP
 }
 
 func TestRvnBootSimple(t *testing.T) {
@@ -75,21 +80,19 @@ func TestRvnBootSimple(t *testing.T) {
 	if err != nil {
 		t.Errorf("Unable to start Raven: %v", err)
 	}
-	// do test here ~~~~~~`
 
-	macAddr := "00:00:00:00:00:01"
-	SetClientMAC("eth1", macAddr)
-	success, mac := CheckClientMAC("eth1", macAddr)
-	if !success {
-		t.Errorf("Client MAC not set! wanted: %v, got: %v", macAddr, mac)
+	// setup the client and server to communicate
+	err = setupClient()
+	if err != nil {
+		t.Errorf("%v", err)
+	}
+	err = setupServer()
+	if err != nil {
+		t.Errorf("%v", err)
 	}
 
-	// get the server IP
-	serverIP := getNodeIP("server")
-	cmd := exec.Command(
-		"ssh -o StrictHostKeyChecking=no -i /var/rvn/ssh/rvn rvn@%s /tmp/code/test/integration/bolt-update", serverIP)
-
-	RunSledc("10.0.0.1")
+	client.RunSledc(shared.ServerIP)
+	// run sledc on client
 
 	// tear down all the raven configuration
 	err = stopRaven()
@@ -157,4 +160,62 @@ func doPing(host string) bool {
 
 	return pings == 2
 
+}
+
+func startRaven() error {
+	// lets start by creating rvn info
+	err := rvn.RunModel()
+	if err != nil {
+		return err
+	}
+	// do libvirt things, atm does not return error
+	rvn.Create()
+
+	// now we can start to use our nodes, bring raven online
+	strErr := rvn.Launch()
+	if len(strErr) != 0 {
+		for _, e := range strErr {
+			log.Println(e)
+		}
+		// TODO: another hack, just return the first of many errors
+		return errors.New(strErr[0])
+	}
+
+	// wait until the nodes are ready to be setup
+	doPingwait()
+
+	// configure the nodes -- set up mounts, again no error
+	rvn.Configure(true)
+
+	// wait until the nodes are ready to be setup
+	doPingwait()
+
+	return nil
+}
+
+func stopRaven() error {
+	err := rvn.Shutdown()
+	if err != nil {
+		for _, e := range err {
+			log.Printf("%v", e)
+		}
+		// FIXME: short hack, just return first of the errors
+		return err[0]
+	}
+
+	// remove all info/details on the raven test configuration
+	rvn.Destroy()
+	return nil
+}
+
+func getNodeIP(node string) string {
+	topo, err := rvn.LoadTopo()
+	if err != nil {
+		log.Fatal(err)
+	}
+	ds, err := rvn.DomainStatus(topo.Name, node)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return ds.IP
 }
